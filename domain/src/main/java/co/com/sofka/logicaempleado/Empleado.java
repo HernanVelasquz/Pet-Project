712a31c753e71c2d9be5d11c807ebf0709a69b75
Stevent Florez <steventflorez94@gmail.com>
2022-06-29 16:16:21 +0200

    arreglo de paquete

---
+++ domain/src/main/java/co/com/sofka/logicaempleado/Empleado.java
@@ -0,0 +1,6 @@
+package co.com.sofka.logicaempleado;
+
+public class Empleado {
+
+
+}
---
+++ domain/src/main/java/co/com/sofka/logicaempleado/comandos/AgregarEmpleado.java
@@ -0,0 +1,6 @@
+package co.com.sofka.logicaempleado.comandos;
+
+import co.com.sofka.domain.generic.Command;
+
+public class AgregarEmpleado extends Command {
+}
